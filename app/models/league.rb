f007a7a47308461b85b2b1f52a60a1fa52a9c2e2
tcharfi66 <charfitej6@gmail.com>
2020-09-26 21:57:38 -0400

    setup

---
+++ app/models/league.rb
@@ -0,0 +1,10 @@
+require_relative '../../config/environment.rb'
+
+
+require_relative "team.rb"
+require_relative "match.rb"
+
+class League < ActiverRecord::Base
+
+asdasdasd
+end
---
+++ app/models/match.rb
@@ -0,0 +1,8 @@
+require_relative '../../config/environment.rb'
+
+
+require_relative "league.rb"
+require_relative "team.rb"
+class Match < ActiveRecord::Base
+asdadasdasd
+end
