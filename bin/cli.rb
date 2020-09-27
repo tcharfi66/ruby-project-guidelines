fc10f4e4e0ace583366691581fff34c46492dcb6
tcharfi66 <charfitej6@gmail.com>
2020-09-26 22:13:09 -0400

    cli and run file frame

---
+++ bin/cli.rb
@@ -0,0 +1,3 @@
+def intro 
+
+end
--- bin/run.rb
+++ bin/run.rb
@@ -1,13 +1,13 @@
 require_relative '../config/environment'
+
+
 require_relative "../lib/league.rb"
 require_relative "../lib/match.rb"
-
 require_relative "../lib/team.rb"
 
 
-welcome
-character = get_character_from_userrequ
-show_character_movies(character)
+intro
+
 
 
 
