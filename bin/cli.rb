25a6621d89aaa35e6159fa08e3fb234053b2fe31
tcharfi66 <charfitej6@gmail.com>
2020-09-28 17:59:20 -0400

    CLI in progress

--- bin/cli.rb
+++ bin/cli.rb
@@ -1,3 +1,16 @@
 def intro 
     puts "Welcome To The Restaurant Directory"
 end
+
+def find_restaurant 
+    puts "Enter Restaurant:  " 
+    name= gets.chomp
+    Restaurant.find_by(name: name)
+
+end
+
+# def restaurant_menu 
+#     RestaurantDish.all.select do |restaurant|
+#         restaurant.restaurant_id == restaurant_id 
+#     end
+# end
--- bin/run.rb
+++ bin/run.rb
@@ -1,12 +1,16 @@
 require_relative '../config/environment'
 
 
-require_relative "../lib/dish.rb"
-require_relative "../lib/restaurant.rb"
-require_relative "../lib/restaurant_dish.rb"
+# require_relative "../app/models/dish.rb"
+# require_relative "../app/models/restaurant.rb"
+# require_relative "../app/models/restaurant_dish.rb"
 
+require_relative "../bin/cli.rb"
 
 intro
+#find_restaurant 
+
+restaurant_menu 
 
 
 
